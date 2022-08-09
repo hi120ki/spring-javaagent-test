cf3a28c817c443052e0f48e607782904d976d5ce
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2022-08-09 20:41:40 +0900

    add Response arg

--- agent/src/main/java/com/example/profile/InstrumentInterceptor.java
+++ agent/src/main/java/com/example/profile/InstrumentInterceptor.java
@@ -2,6 +2,7 @@ package com.example.profile;
 
 import java.util.concurrent.Callable;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import net.bytebuddy.implementation.bind.annotation.Argument;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import net.bytebuddy.implementation.bind.annotation.SuperCall;
@@ -11,12 +12,14 @@ public class InstrumentInterceptor {
   @RuntimeType
   public static Object intercept(
       @Argument(0) HttpServletRequest request,
+      @Argument(1) HttpServletResponse response,
       @SuperCall Callable<?> callable) {
     System.out.println("[1] request : " + request.getMethod());
     try {
       System.out.println("[2] request : " + request.getMethod());
       Object ret = callable.call();
       System.out.println("[3] request : " + request.getMethod());
+      System.out.println("[1] response : " + response.getContentType());
       return ret;
     } catch (Exception e) {
       System.out.println("Exception :" + e.getMessage());
